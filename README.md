a978ed3000b387cd361282faba0f049462d183c8
Jan Stümmel <jan@ubari.de>
2022-07-04 18:09:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# aws role switch
+
+A browser plugin to quickly switch aws roles.
+
+## Development
+
+```sh
+npm i
+npm start # start dev server
+npm bundle # build distribution zip
+```
---
+++ src/options/options.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CodeMirror from '@uiw/react-codemirror'
+import { render } from 'react-dom'
+import { StreamLanguage } from '@codemirror/language'
+import { toml } from '@codemirror/legacy-modes/mode/toml'
+import 'minireset.css'
+
+const App = () => {
+  return (
+    <div>
+      <div className="header">
+        <div>Config</div>
+        <div><button>Save</button></div>
+      </div>
+      <CodeMirror
+        minHeight="300px"
+        theme="dark"
+        extensions={[StreamLanguage.define(toml)]} />
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
