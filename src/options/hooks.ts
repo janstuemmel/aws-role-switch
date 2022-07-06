26fa92bd85fca903dbc511bdc551a3d486b02686
Jan Stümmel <jan@ubari.de>
2022-07-06 23:00:23 +0200

    add options ui styling

---
+++ src/options/hooks.ts
@@ -0,0 +1,45 @@
+import {
+  useEffect,
+  useState,
+  Dispatch,
+  SetStateAction
+} from 'react';
+import { marked } from 'marked'
+import { getConfig } from '../common/config';
+
+// @ts-ignore
+import readme from 'url:../../README.md';
+
+export const useDocs = () => {
+  const [docs, setDocs] = useState('')
+  useEffect(() => {
+    fetch(readme)
+      .then(res => res.text())
+      .then(txt => marked(txt))
+      .then(setDocs)
+  }, [])
+  return docs
+}
+
+export const useColorScheme = () => {
+  const matchesDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useState<'dark' | 'light'>(matchesDark ? 'dark' : 'light');
+  const listener = (event: MediaQueryListEvent) => setTheme(() => event.matches ? 'dark' : 'light');
+  
+  useEffect(() => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', listener);
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', listener);
+    }
+  }, []);
+
+  return theme;
+};
+
+export const useConfigFile = (): [string, Dispatch<SetStateAction<string>> ] => {
+  const [configFile, setConfigFile] = useState('')
+  useEffect(() => {
+    getConfig().then(setConfigFile)
+  }, [])
+  return [configFile, setConfigFile]
+};
--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,35 +1,57 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import CodeMirror from '@uiw/react-codemirror'
 import { StreamLanguage } from '@codemirror/language'
 import { toml } from '@codemirror/legacy-modes/mode/toml'
 
-import { getConfig, setConfig } from '../common/config'
+import { setConfig } from '../common/config'
 import keymap from './keymap'
+import { 
+  useColorScheme,
+  useConfigFile,
+  useDocs 
+} from './hooks'
 
 const App = () => {
-  const [configFile, setConfigFile] = useState<string | undefined>(undefined)
+  const [configFile, setConfigFile] = useConfigFile()
+  const docs = useDocs();
+  const theme = useColorScheme()
+
   const onSave = () => setConfig(configFile || '')
   const editorKeymap = keymap([{ key: 'Ctrl-s', fn: onSave }])
 
-  useEffect(() => {
-    getConfig().then(setConfigFile)
-  }, [])
-
   return (
-    <div>
-      <div className="header">
-        <div>Config</div>
-        <div>
-          <button onClick={onSave}>Save</button>
-        </div>
+    <div id="options-ui" className="wrapper" data-color-mode="dark">
+      <div className="editor">
+        <CodeMirror
+          height="100vh"
+          theme={theme}
+          onChange={(val) => setConfigFile(val)}
+          value={configFile ? configFile : ''}
+          extensions={[StreamLanguage.define(toml), editorKeymap]} 
+        />
       </div>
-      <CodeMirror
-        minHeight="300px"
-        theme="dark"
-        onChange={(val) => setConfigFile(val)}
-        value={configFile ? configFile : ''}
-        extensions={[StreamLanguage.define(toml), editorKeymap]} />
+      <aside className="">
+        <div className="terminal-nav">
+          <div className="terminal-logo">
+            <div className="logo">
+              <a href="#" className="no-style">AWS_Config</a>
+            </div>
+          </div>
+          <nav className="terminal-menu">
+            <ul>
+              <li><a className="menu-item" href="#">Issues</a></li>
+              <li><a className="menu-item" href="#">Github</a></li>
+              <li><a className="menu-item" href="#">Homepage</a></li>
+            </ul>
+          </nav>
+        </div>
+        <button className="btn btn-block">
+          Save config (Ctrl-s)
+        </button>
+        <hr />
+        <p dangerouslySetInnerHTML={{ __html: docs }} />
+      </aside>
     </div>
   )
 }
