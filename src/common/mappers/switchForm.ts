19da838c29a5bed8e55481860555a3d73478ca4d
Jan Stümmel <jan@ubari.de>
2022-08-01 00:04:12 +0200

    revert: revert switchForm.ts

--- src/common/mappers/switchForm.ts
+++ src/common/mappers/switchForm.ts
@@ -4,11 +4,11 @@ export const mapToSwitchForm = (
   configItem: AWSConfigItem, 
   args: Args,
 ): SwitchRoleForm => ({
-    account: configItem.aws_account_id,
-    roleName: configItem.role_name,
-    color: configItem.color?.replace('#', ''),
-    displayName: `${configItem.title} | ${configItem.aws_account_id}`.slice(0, 64),
-    action: 'switchFromBasis',
-    mfaNeeded: '0',
-    ...args,
-  });
+  account: configItem.aws_account_id,
+  roleName: configItem.role_name,
+  color: configItem.color?.replace('#', ''),
+  displayName: `${configItem.title} | ${configItem.aws_account_id}`.slice(0, 64),
+  action: 'switchFromBasis',
+  mfaNeeded: '0',
+  ...args,
+});
