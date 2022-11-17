cb9655612305d2f5f30d3bee9d5967455f527501
Jan Stümmel <jan@ubari.de>
2022-11-17 22:45:23 +0100

    feat: Enhance build
    * Add version to merge manifests util
    * Add deploy workflow
    * Add release-it lib

--- src/build/plugins/mergeManifestsWithVersion.ts
+++ src/build/plugins/mergeManifestsWithVersion.ts
@@ -8,20 +8,20 @@ const getPath = (fileName: string) => path.join(process.cwd(), fileName);
 const readJsonFiles = (files: string[]) => 
   files.map((f) => fs.promises.readFile(getPath(f), 'utf8').then(JSON.parse));
 
-export const onBuildEnd = (jsonPaths: string[], outdir: string) => async () => {
+export const onBuildEnd = (version: string, jsonPaths: string[], outdir: string) => async () => {
   const files = await Promise.allSettled(readJsonFiles(jsonPaths))
     .then(res => res.filter((item) => item.status !== 'rejected') as PromiseFulfilledResult<object>[])
     .then(res => res.map((item) => item.value));
   if (jsonPaths.length > 0) {
-    const json = JSON.stringify(Object.assign({}, ...files), null, 2);
+    const json = JSON.stringify(Object.assign({}, ...files, { version }), null, 2);
     const fileName = path.basename(jsonPaths[0]);
     await fs.promises.writeFile(path.join(process.cwd(), outdir || '', fileName), json);
   }
 };
 
-export const mergeJsons = (jsonPaths: string[]): Plugin => ({
-  name: 'mergeJsonsPlugin',
+export const mergeManifestWithVersion = (version: string, jsonPaths: string[]): Plugin => ({
+  name: 'mergeManifestsWithVersionPlugin',
   setup: (build) => {
-    build.onEnd(onBuildEnd(jsonPaths, build.initialOptions.outdir || ''));
+    build.onEnd(onBuildEnd(version, jsonPaths, build.initialOptions.outdir || ''));
   },
 });
