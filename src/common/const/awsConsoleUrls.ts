08e10ce3ea0d861de49fb3adaa06bced5c394862
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2022-11-26 15:41:19 +0100

    fix: add lightsail and health dashboards to consoleUrls (#40)

--- src/common/const/awsConsoleUrls.ts
+++ src/common/const/awsConsoleUrls.ts
@@ -1,6 +1,14 @@
 export default [
+  // standard console
   'https://*.console.aws.amazon.com/*',
-  'https://phd.aws.amazon.com/*',
   'https://*.console.amazonaws-us-gov.com/*',
-  'https://*.console.amazonaws.cn/*'
+  'https://*.console.amazonaws.cn/*',
+
+  // lightsail
+  "https://lightsail.aws.amazon.com/*",
+  
+  // health dashboard 
+  "https://health.aws.amazon.com/*",
+  "https://phd.amazonaws-us-gov.com/*",
+  "https://phd.amazonaws.cn/*"
 ];
