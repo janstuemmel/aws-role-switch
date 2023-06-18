04bca668501ec8adbc5e82b235a45bcaf030e28e
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-06-18 16:41:12 +0200

    feat: Add firefox manifest v3

--- src/manifest.common.ts
+++ src/manifest.common.ts
@@ -3,7 +3,31 @@ import { version } from '../package.json';
 
 export default {
   version: version,
+  manifest_version: 3,
   name: "AWS role switch",
+  action: {
+    default_title: "AWS role switch",
+    default_popup: "popup/popup.html",
+    browser_style: true,
+    default_icon: {
+      128: "assets/icon128.png"
+    }
+  },
+  commands: {
+    _execute_action: {
+      description: "Open the popup window",
+      suggested_key: {
+        default: "Ctrl+Shift+L"
+      }
+    }
+  },
+  permissions: [
+    "storage",
+    "cookies",
+  ],
+  host_permissions: [
+    ...awsConsoleUrls,
+  ],
   description: "Quickly switch between roles on the AWS console",
   homepage_url: "https://github.com/janstuemmel/aws-role-switch",
   options_ui: {
@@ -30,5 +54,11 @@ export default {
       ],
       run_at: "document_idle"
     }
-  ]
+  ],
+  browser_specific_settings: {
+    gecko: {
+      id: "{31f7b254-7ac9-4f3a-ae3c-ef67ea153e4a}",
+      strict_min_version: "57.0"
+    }
+  },
 };
