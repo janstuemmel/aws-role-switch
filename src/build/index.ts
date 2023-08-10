8b397080f5fa64a7661e1bcb8dd270ca58023d7d
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-08-11 00:39:01 +0200

    fix: remove build config file
    closes #243

--- src/build/index.ts
+++ src/build/index.ts
@@ -1,27 +1,59 @@
-import { context } from 'esbuild';
-
 import {
-  pagesConfig,
-  scriptsConfig,
-} from './config';
+  context,
+  BuildOptions,
+} from 'esbuild';
+import { copy } from 'esbuild-plugin-copy';
+import { sassPlugin } from 'esbuild-sass-plugin';
+
 import { writeManifest } from "./plugins";
 import manifestFirefox from '../manifest.firefox';
 import manifestChrome from '../manifest.chrome';
-
 type Target = 'firefox' | 'chrome';
 
+const defaultConfig: BuildOptions = {
+  bundle: true,
+  target: 'ES2018',
+  format: 'cjs',
+  sourcemap: 'external',
+  loader: {
+    '.svg': 'file',
+    '.md': 'file',
+  },
+  metafile: true,
+};
+
 const buildPages = (target: Target, minify: boolean) => context({
-  ...pagesConfig,
+  ...defaultConfig,
   minify,
-  outdir: `dist/${target}`,  
+  outdir: `dist/${target}`,
+  entryPoints: [
+    'src/popup/popup.tsx',
+    'src/popup/popup.scss',
+    'src/options/options.tsx',
+    'src/options/options.scss',
+  ],
   plugins: [
-    ...pagesConfig.plugins ?? [],
+    sassPlugin(),
+    copy({
+      assets: [{
+        from: ['src/assets/*'],
+        to: ['./assets']
+      }, {
+        from: ['src/**/*.html'],
+        to: ['.']
+      }],
+    }),
     writeManifest(target === 'firefox' ? manifestFirefox : manifestChrome),
-  ]
+  ],
 });
 
 const buildScripts = (target: Target) => context({
-  ...scriptsConfig,
+  minify: false,
+  entryPoints: [
+    'src/content/aws-console.ts',
+    'src/content/aws-signin.ts',
+    'src/background/background.ts',
+  ],
   outdir: `dist/${target}`
 });
 
